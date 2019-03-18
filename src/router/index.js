4310a23e05e8de9864c5edb81bb2b379a53d9db2
longfei <974638684@qq.com>
2019-03-18 14:26:34 +0800

    ”“

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
-
+import home from '@/components/home/home'
+import store from '@/components/store/store'
+import live from '@/components/live/live'
+import mine from '@/components/mine/mine'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      // path: '/'
-      // name: 'HelloWorld'
-      // component: HelloWorld
+      path: '/',
+      name: 'home',
+      component: home
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: home
+    },
+    {
+      path: '/store',
+      name: 'store',
+      component: store
+    },
+    {
+      path: '/live',
+      name: 'live',
+      component: live
+    },
+    {
+      path: '/mine',
+      name: 'mine',
+      component: mine
     }
   ]
 })
